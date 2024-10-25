8fce82d967b3aa5e0a2ffe3e1df670d09a39441d
xxvintex <xxvintex32@gmail.com>
2024-10-25 19:12:17 +0300

    game.js

--- game.js
+++ game.js
@@ -1,52 +1,64 @@
 class ClickerGame {
     constructor() {
-        this.score = 0;         
-        this.clickValue = 1;      
-        this.autoClickValue = 0; 
+        this.score = 0;
+        this.clickValue = 1;
+        this.autoClickValue = 0;
         this.upgrades = [
-            { name: "Подвійний клік", cost: 100, clickBonus: 1, autoClickBonus: 0, level: 0 },
-            { name: "Автоматичний клікер", cost: 500, clickBonus: 0, autoClickBonus: 1, level: 0 }
-        ];      
-        this.interval = null;    
-        this.loadGame();         
-        this.displayUpgrades();  
+            { name: "Подвійний клік", baseCost: 100, cost: 100, clickBonus: 1, autoClickBonus: 0, level: 0 },
+            { name: "Автоматичний клікер", baseCost: 500, cost: 500, clickBonus: 0, autoClickBonus: 1, level: 0 }
+        ];
+        this.costMultiplier = 1.15; 
+        this.interval = null;
+        this.loadGame();
+        this.displayUpgrades();
     }
 
-   
     click() {
         this.score += this.clickValue;
         this.updateUI();
     }
 
-   
     buyUpgrade(upgrade) {
-        if (this.score >= upgrade.cost) {
+        if (this.score >= upgrade.cost && !isNaN(upgrade.cost)) {
             this.score -= upgrade.cost;
             upgrade.level++;
             this.clickValue += upgrade.clickBonus;
             this.autoClickValue += upgrade.autoClickBonus;
+
+            
+            const newCost = Math.floor(upgrade.baseCost * Math.pow(this.costMultiplier, upgrade.level));
+            console.log(`Upgrade: ${upgrade.name}, new calculated cost: ${newCost}, previous cost: ${upgrade.cost}`);
+
+           
+            upgrade.cost = newCost;
+
+            
+            if (isNaN(upgrade.cost) || upgrade.cost === undefined) {
+                console.error(`Error: Upgrade cost became invalid (NaN/undefined) for ${upgrade.name}`);
+                upgrade.cost = upgrade.baseCost; 
+            }
+
             this.updateUI();
-            this.displayUpgrades(); 
-            this.startAutoClick(); 
+            this.displayUpgrades();
+            this.startAutoClick();
+        } else {
+            alert("Не вистачає очок для покупки!");
         }
     }
 
-    
     startAutoClick() {
         if (this.autoClickValue > 0 && !this.interval) {
             this.interval = setInterval(() => {
                 this.score += this.autoClickValue;
                 this.updateUI();
-            }, 1000); 
+            }, 1000);
         }
     }
 
-    
     updateUI() {
         document.getElementById("score").innerText = `Очки: ${this.score}`;
     }
 
-   
     saveGame() {
         const saveData = {
             score: this.score,
@@ -58,7 +70,6 @@ class ClickerGame {
         alert("Прогрес збережено!");
     }
 
-    
     loadGame() {
         const savedData = localStorage.getItem("clickerGameSave");
         if (savedData) {
@@ -66,17 +77,20 @@ class ClickerGame {
             this.score = score;
             this.clickValue = clickValue;
             this.autoClickValue = autoClickValue;
-            this.upgrades = upgrades;
-            this.updateUI();
-            this.startAutoClick(); 
+            this.upgrades = upgrades.map((upg, index) => ({
+                ...upg,
+                baseCost: this.upgrades[index].baseCost, 
+                cost: upg.cost || Math.floor(this.upgrades[index].baseCost * Math.pow(this.costMultiplier, upg.level)) 
+            }));
+            console.log('Loaded upgrades:', this.upgrades); 
+            this.startAutoClick();
         }
     }
 
-   
     displayUpgrades() {
         const upgradeContainer = document.getElementById("upgrades");
-        upgradeContainer.innerHTML = ''; 
-        this.upgrades.forEach((upgrade, index) => {
+        upgradeContainer.innerHTML = '';
+        this.upgrades.forEach(upgrade => {
             const upgradeButton = document.createElement("button");
             upgradeButton.innerText = `${upgrade.name} (Ціна: ${upgrade.cost}, Рівень: ${upgrade.level})`;
             upgradeButton.addEventListener("click", () => this.buyUpgrade(upgrade));
@@ -92,3 +106,4 @@ const game = new ClickerGame();
 document.getElementById("clickButton").addEventListener("click", () => game.click());
 document.getElementById("saveButton").addEventListener("click", () => game.saveGame());
 document.getElementById("loadButton").addEventListener("click", () => game.loadGame());
+
